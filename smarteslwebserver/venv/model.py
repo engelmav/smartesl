b1890c96231c3f2c91666ea25b08b31d64123948
Vincent Engelmann <vengelmann@pragmatrading.com>
2015-05-05 20:41:21 -0400

    primitive timeline searching working.

--- smarteslwebserver/venv/model.py
+++ smarteslwebserver/venv/model.py
@@ -99,6 +99,34 @@ class DBAccessor:
             print e.pgcode
         return results
 
+    def searchTimelines(self,searchPhrase):
+        searchSql = """
+            select sid from
+                ( select
+                    qs.set_id as sid,
+                    to_tsvector(qs.set_name) || ' ' ||
+                    to_tsvector(u.username) as document
+                  from question_sets qs join users u on qs.creator_id = u.user_id) as doc
+            where doc.document @@ to_tsquery(%s);
+            """
+        results = ''
+        try:
+            self.cur.execute(searchSql,(searchPhrase,))
+            results = self.cur.fetchall()
+        except psycopg2.Error as e:
+            self.conn.rollback()
+            print e.pgcode
+        return results
+
+    def setCurrentQuestion(self,qid):
+        #         smartesl=# \d current_questions
+        #  Table "public.current_questions"
+        #    Column    |  Type   | Modifiers
+        # -------------+---------+-----------
+        #  question_id | integer |
+        #  class_id    | integer |
+        pass
+
     def addTimeline(self, timelineData):
         qIds =     timelineData['questionIds']
         tlName =   timelineData['timelineName']
