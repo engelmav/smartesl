52b50f450067137abff2917911ed5ea32433a83c
Vincent Engelmann <vengelmann@pragmatrading.com>
2015-12-20 16:34:17 -0500

    Changed order of result size check and removing exception, adding password param

--- smarteslwebserver/venv/user_login.py
+++ smarteslwebserver/venv/user_login.py
@@ -1,4 +1,4 @@
-import settings  
+import settings
 import sql_lib as sql
 from db_accessor import DBAccessor
 from jsonable import Jsonable
@@ -16,17 +16,19 @@ class UserLogin(DBAccessor,Jsonable):
         """Cause a user to be "logged in". Will lookup user and if present,
         create a login session for the user.
         """
-        self.cur.execute( sql.get_user_data, (username,) )
+        self.cur.execute( sql.get_user_data, (username, password) )
         results = self.cur.fetchall()
-        self.userName, self.firstName, self.lastName, self.role = results[0]
-        self.views = self._getViews()
         resultLength = len(results)
         if resultLength == 1:
             self.session_id = str(uuid.uuid4())
             return self
 
         else:
-            raise Exception("Invalid username %s!", username)
+            print "Invalid username %s!" % username
+            return None
+
+        self.userName, self.firstName, self.lastName, self.role = results[0]
+        self.views = self._getViews()
 
     def _getViews(self):
         self.cur.execute( sql.get_views_for_user, (self.userName,) )
