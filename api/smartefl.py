656d23aa187b5257a5772690713c356b1cd58a24
Vincent Engelmann <vengelmann@solarcity.com>
2017-05-26 16:16:22 -0700

    re-working into flask-restful

--- api/smartefl.py
+++ api/smartefl.py
@@ -1,8 +1,20 @@
-from eve import Eve
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
 
 
-app = Eve()
+class MultipleChoiceQuestion(Resource):
+    def get(self, question_id):
+        return {'hello': 'world'}, 200
 
+    def post(self):
+        pass
+
+
+api.add_resource(MultipleChoiceQuestion, '/multiple_choice')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5999)
+    
