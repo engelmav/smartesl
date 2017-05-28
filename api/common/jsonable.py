e29a9367a5e94866096b7c18d2cf5ff73f1f3e93
Vincent Engelmann <vengelmann@solarcity.com>
2017-05-28 14:54:38 -0700

    removing old model skeletons and json utilities

--- api/common/jsonable.py
+++
@@ -1,9 +0,0 @@
-import json
-class Jsonable(object):
-    def to_json(self,vars_list):
-        fields = vars(self)
-        returnable_fields = {}
-        for field in fields:
-            if field in vars_list:
-                returnable_fields[field] = fields[field]
-        return json.dumps(returnable_fields)
--- api/settings.py
+++
@@ -1,1 +0,0 @@
-DB_CXN_STR = "dbname=smartesl user=appuser password=a0kroger host=localhost port=15432"
