38ebf7d173dfd72510fc43d44de2f8e15fcc14c8
Vincent Engelmann <vengelmann@solarcity.com>
2017-06-07 11:39:56 -0700

    namespacing services in test

--- api/test/test_services.py
+++ api/test/test_services.py
@@ -1,27 +1,31 @@
-from services import create_user, get_user_by_id, create_multi_choice, \
-    create_question_set
+import services as s
 from .seed_data import dummy_question, dummy_question2, dummy_user
 
 
 def test_find_user_by_id():
-    user_result = get_user_by_id(108)
+    user_result = s.get_user_by_id(108)
     assert user_result.user_id == 108
 
 
 def test_create_question():
-    q = create_multi_choice(dummy_question)
+    q = s.create_multi_choice(dummy_question)
     assert q is not None
 
 
 def test_create_user():
-    u = create_user(dummy_user)
+    u = s.create_user(dummy_user)
     assert u is not None
+
+
+def test_get_question_set():
+    u = s.get_question_set_by_id(7)
+    assert u.question_set_id == 7
 
 
 def test_create_question_set():
     question_list = [
-        create_multi_choice(dummy_question).question_id,
-        create_multi_choice(dummy_question2).question_id
+        s.create_multi_choice(dummy_question).question_id,
+        s.create_multi_choice(dummy_question2).question_id
     ]
 
     set_name = 'Test Question Set!'
@@ -32,8 +36,6 @@ def test_create_question_set():
         'questions': question_list
     }
 
-    qs = create_question_set(question_set)
+    qs = s.create_question_set(question_set)
     assert qs.set_name == set_name
-
-
 
