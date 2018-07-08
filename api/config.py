cb10cb0ece13653a4ad4de4ca70a27376aafe380
Vincent Engelmann <vincent.engelmann1@gmail.com>
2018-07-07 17:26:54 -0700

    ci build env var for settings

--- api/config.py
+++ api/config.py
@@ -2,19 +2,23 @@ import os
 import json
 from logger import log
 
-config_path = os.getenv('SMARTEFL_CONFIG')
 
-if config_path is None:
-    raise RuntimeError('You must specify your config file path'
-                       'by setting SMARTEFL_CONFIG.')
+is_ci_build = os.getenv('CI_BUILD')
 
-environment = 'test'
-log.debug("Using config from %s", config_path)
+if is_ci_build == None:
+    config_path = os.getenv('SMARTEFL_CONFIG')
 
-with open(config_path) as f:
-    config_str = f.read()
-    config_json = json.loads(config_str)
-    db_conn_str = config_json[environment]['db_connection_str']
+    if config_path is None:
+        raise RuntimeError('You must specify your config file path'
+                        ' by setting SMARTEFL_CONFIG.')
 
+    environment = 'test'
+    log.debug("Using config from %s", config_path)
 
-
+    with open(config_path) as f:
+        config_str = f.read()
+        config_json = json.loads(config_str)
+        db_conn_str = config_json[environment]['db_connection_str']
+else:
+    log.debug("Initializing in CI pipeline")
+    db_conn_str = "ci_smartefl_db"
